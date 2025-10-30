5e476e35cb81b9d78f4e71e71f59609fa7af075e
joelchirwa <joelchirwa@my.uopeople.edu>
2025-10-30 10:18:08 +0200

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,4 +1,14 @@
 import express from 'express';
+import mangoose from 'mongoose'; 
+import dotenv from 'dotenv';
+
+dotenv.config();   
+
+mangoose.connect(process.env.MONGO_URI).then(() => {
+    console.log("Connected to MongoDB");
+}).catch((error) => {
+    console.log("Error connecting to MongoDB:", error);
+});
 
 const app = express();
 
